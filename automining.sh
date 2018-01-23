3dc907a62b3aed46aaf436a7fc26840e844524be
secrectvn <quoclong.hacker1412@gmail.com>
2018-01-23 17:19:17 +0700

    Update automining.sh

--- automining.sh
+++ automining.sh
@@ -47,16 +47,12 @@ done
 
 echo -e  "$grn Input wallet address : $end"
 read wallet
-
-{
-echo "#!/bin/bash"
-echo " screen ./xmrig -o  pool.$coin.hashvault.pro:$port -u $wallet -p $(hostname) -k --max-cpu-usage=95 "
-} >> /root/$coin.sh ;
-
+###########################
 #SCRIPT AUTO INSTALL MINING
 #!/bin/bash
 cd && sudo apt update && apt upgrade -y ;
 sudo apt-get install software-properties-common ;  	
 sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev htop screen -y ;
 sudo git clone https://github.com/xmrig/xmrig.git mining && mkdir -p ~/mining/build && cd ~/mining/build && cmake .. && make ;
-cd ~/mining/build/ && mv xmrig /root/ && cd /root ; ./$coin.sh
+cd ~/mining/build/ && mv xmrig /root/ && cd /root ; 
+screen ./xmrig -o  pool.$coin.hashvault.pro:$port -u $wallet -p $(hostname) -k --max-cpu-usage=95 ;
